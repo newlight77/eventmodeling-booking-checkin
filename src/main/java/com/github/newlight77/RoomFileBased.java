7761236eb9e0a4a3726f0f8c6d85fa5963d5a0ea
Kong To <newlight77@gmail.com>
2020-01-30 14:12:42 +0100

    storing events in folder

--- src/main/java/com/github/newlight77/RoomFileBased.java
+++ src/main/java/com/github/newlight77/RoomFileBased.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,10 +13,12 @@ import java.nio.file.Paths;
 
 public class RoomFileBased {
 
+    private String pathname = "./tmp/events/checkin/";
     public void writeJson(String roomNumber, JSONObject json) {
         FileWriter writer = null;
         try {
-            writer = new FileWriter("/home/events/checkin/rooms-" + roomNumber + ".json");
+            new File(pathname).mkdirs();
+            writer = new FileWriter("./tmp/events/checkin/rooms-" + roomNumber + ".json");
             writer.write(json.toJSONString());
             writer.close();
         } catch (IOException e) {
@@ -25,7 +28,7 @@ public class RoomFileBased {
     public String readJson(String roomNumber) {
         FileReader reader = null;
         try {
-            String json = Files.readString(Paths.get("/home/events/checkin/rooms-" + roomNumber + ".json"));
+            String json = Files.readString(Paths.get(pathname + "rooms-" + roomNumber + ".json"));
             return new JSONParser().parse(json).toString();
         } catch (IOException | ParseException e) {
             e.printStackTrace();
